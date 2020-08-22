3c886d2dedb4a9f58e3dc2dd715fae05e8c753cd
falen <u.kerja2020@gmail.com>
2020-08-22 14:37:01 +0700

    game

--- desktop/menu/tangkas.php
+++ desktop/menu/tangkas.php
@@ -1,55 +1,49 @@
 	<?php 
 	include '../template/header.php'; 
-	?>	
-    <div class="cockfightpage">
-        <div class="container">
-            <img src="../../resources/images/desktop/others_banner.jpg" alt="" />
+	?>
+	<div class="sbsports">
+	    <div class="container">
+	        <div class="row">
+	            <div class="col-md-12">
+	                <div class="banner_section">
+	                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+	                        <ol class="carousel-indicators">
 
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="cockfight_main">
-                        <ul id="ctl00_ContentPlaceHolder1_ulGame">
-                            <li class="imagesec">
-								<img src="../../resources/images/desktop/game.png" />
-								<span class="btmlink_orange"><a href="register.php">DAFTAR</a></span>
-                            </li>
-                            <li 
-									class="imagesec"><img src="../../resources/images/desktop/game.png" /><span
-                                    class="btmlink_orange"><a href="register.php">DAFTAR</a></span>
-                            </li>
-							<li 
-									class="imagesec"><img src="../../resources/images/desktop/game.png" /><span
-                                    class="btmlink_orange"><a href="register.php">DAFTAR</a></span>
-                            </li>
-						
-                        </ul>
-                    </div>
-					
-					<div class="row">
-                <div class="col-md-12">
-                    <div class="cockfight_main">
-                        <ul id="ctl00_ContentPlaceHolder1_ulGame">
-                            <li class="imagesec">
-								<img src="../../resources/images/desktop/game.png" />
-								<span class="btmlink_orange"><a href="register.php">DAFTAR</a></span>
-                            </li>
-                            <li 
-									class="imagesec"><img src="../../resources/images/desktop/game.png" /><span
-                                    class="btmlink_orange"><a href="register.php">DAFTAR</a></span>
-                            </li>
-							<li 
-									class="imagesec"><img src="../../resources/images/desktop/game.png" /><span
-                                    class="btmlink_orange"><a href="register.php">DAFTAR</a></span>
-                            </li>
-						
-                        </ul>
-                    </div>
-            </div>
-            <div class="row">
-                <br />
-            </div>
-        </div>
-    </div>
-	<?php 
+	                        </ol>
+	                        <div class="carousel-inner">
+
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+	        <div class="row">
+	            <div class="col-md-12">
+	                <div class="sbsports_tabbing">
+	                    <div class="sbsports_tabbing1">
+	                        <ul id="ctl00_ContentPlaceHolder1_ulGame">
+	                            <li class="imagesec"><img src="../../resources/images/desktop/game.png" /><a
+	                                    href="javascript:alert('Please login.');" class="bettingnow">DAFTAR</a></li>
+	                            <li class="imagesec"><img src="../../resources/images/desktop/game.png" /><a
+	                                    href="javascript:alert('Please login.');" class="bettingnow">DAFTAR</a></li>
+	                            <li class="imagesec"><img src="../../resources/images/desktop/game.png" /><a
+	                                    href="javascript:alert('Please login.');" class="bettingnow">DAFTAR</a></li>
+								<li class="imagesec"><img src="../../resources/images/desktop/game.png" /><a
+	                                    href="javascript:alert('Please login.');" class="bettingnow">DAFTAR</a></li>
+								<li class="imagesec"><img src="../../resources/images/desktop/game.png" /><a
+	                                    href="javascript:alert('Please login.');" class="bettingnow">DAFTAR</a></li>
+								<li class="imagesec"><img src="../../resources/images/desktop/game.png" /><a
+	                                    href="javascript:alert('Please login.');" class="bettingnow">DAFTAR</a></li>
+	                            
+	                        </ul>
+	                    </div>
+	                    <hr class="spacer10px" />
+	                    <div class="sbsports_tabbing2">
+	                        <img src="../../resources/images/desktop/win88-icon.png" alt="" />
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+	        <?php 
 	include '../template/footer.php'; 
 	?>
