7130e14561a3cfbbeb82143e9b5468c9150b02d7
falen <u.kerja2020@gmail.com>
2020-08-27 12:00:39 +0700

    mobile + fix footer desktop

---
+++ mobile/menu/about-us.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+    <title>LIGAMANSION</title>
+    <meta name="description" content="LIGAMANSION memberikan bonus terbaik disetiap game" />
+    <meta name="theme-color" content="Red" />
+    <meta name="language" content="id-ID" />
+    <meta name="categories" content="website" />
+    <meta name="robots" content="INDEX,FOLLOW" />
+	
+    <!--<link rel="shortcut icon" type="image/x-icon" href="https://api2-fz8.tr8games.com/images/favicon_96c00afb-2339-4071-aabc-5685e6905423_1597744944117.png?1597744944117" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">-->
+    <link href="../../resources/css/desktop/bootstrap.css" rel="stylesheet" type="text/css" />
+    <link href="../../resources/css/desktop/style.css" rel="stylesheet" type="text/css" />
+    <link href="../assets/fontawesome-free-5.14.0-web/css/all.min.css" rel="stylesheet" type="text/css" />
+    <link type="text/css" rel="stylesheet" href="../../resources/css/endlessRiver.css" />
+    <link href="../../resources/css/jquery.range.css" rel="stylesheet" />
+    <link href="../../resources/css/desktop/main.css" rel="stylesheet" type="text/css">
+    <link href="../assets/lobibox-master/dist/css/lobibox.css" rel="stylesheet" type="text/css">
+	
+	<script src="../../resources/js/jquery.min.js"></script>
+
+
+    <title>
+
+    </title>
+</head>
+
+<body>
+    <form name="aspnetForm" method="post" action="./about-us" id="aspnetForm">
+        <div>
+            <input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE"
+                value="r8r9fDLG4S07iYs6uyyVMbuH53jnSC4T/jkH+qEN+AZB6cUCwgMN0+t5YQq4rqK/mh0Hu+F00rC0AkP3lOiSdvYTVS92Q+2oQWWW9ufDvQ/fKNzpdN19rdWJ0jE6QTKx808uxpEJg+whwLFo45TC+StqlaDOGZVnIP/N56/YMNJCCCPQsQLuxNcAdar2ol7LXApaikfMOm1fecseJZGYGKVSW0EYiixdS53T+sgWyJYTfeCvTf3iNen930MBB0oaUtCnA6JtTqpaBQE2KiWCgQnQ12jx3+qtrveGyrL6wk8zjE0VOaOWW/K8lOTjxqA0V/N4lGfdskG/3U+evP0gT00K9dJ0V8xPz0luM0rk0nQPycsKpsqNQ3MssfFBib3ZWyzZDTjNLpEiH66QO9Ce/8MXpYB47Ydj1llcoeUWU2TAXClXQ8HPURHi5iN5467nsujJImSkmZkpTwtp+6Hfc+4wq+Yoso2WXkPttKUCtPCcxisGuke74BUwogL0SeujCM1e42cJjm3lfkkes1V7jhSY6BTczOmKstc+hPAa7nu/HdI+qz0Uc1blcK2ZKsu1UP5xlfjYuXTGN2B1KApp2RWvDtAAQ+IEXeMmaqwJK4xNJox4Eu4XARkj637jP8Q4raGtMacp9XCBG1PqU4blRnWwsryVao1DprvfB+0H7LYoe2KthO3F82KtNNamxd85d6iDyCmBBSm0CbgQLWOhjhjjz2bdY9jI43SNl3+AZ1eFdokVcDL7NgjJOMT6oQXxEExaHP0KIiC0sROh4fqDsGv/ewGFdNAbitztWRVju1iYHXbOq8JIm8LO9MZuSIsXKExUzxuWAEn/DWL2zWBq1lWQUuLS7HMLE5QuuuClA6J9BbcyMu6eM/x/I1n7uy3iDRQxEU+l5qgYf7z73jFuROE/71SckRP8BaBiQz31oL/yljdJouX8TlpbW4/KepayYAT40YxtCVXJBqH0cwPnF3WAatkvEk/DpmF0XLfH4UnGHjhs0XJ1HXjIRaFiAU0piJfv9j4gasuj2tOqd6KxptkdnPAEHxg9k4uV83emamHzCYQVuUD5qOL3hCZJgeBEj1Wy0Zj0/ueYl4XbPUOG1wcrxsYJcrv2AqB3SkYwCLPzIp2u+YpkTAOKGvmcOqF5gkdgBdgdj6v+8j+JptRZl1E9EOFNtm1TdGq//oGIlIO9Ivvvs5wLy5s66hu8kL6gB3wEUtnrOcVphRKDE4wX4NNMIRAw05pJbFqVW0IbSM1ebmSMA/EPRnkHbzhTgJ+/B1j1T4qrdpiEmatfbH2zVERGC67rDWZ1JqgpNc5uPyrr0xB4aLmvUzTKhp7ZUkTEpSY+8TxwhzuZn9NcIYbgE4RXRLsUSDR/id6PlsMPGm84/lOmF/Qp5aC/PHAmPiPAn+tF+B4glMpfdaJw9jwdqgCdVLrtNVmTbSadmPwLDobCGJy+LoBeFgVvj5Cg2NuUIqN1y/4O4zfE8kjrr8EQBFbF/sMx6OOYfwQR7YPNT/rzyETSGKvXx2O++RuE52vT1cqk0N1KPxtzrbIYYrbp5/ojnIn2TtJT50EWk9EdxoeQfVFltnabbogmMn1HebM6srm0rQd8T6m2ynJ5pc1sU3aUVa1G4zLyhP6uzZop4b+xLN63cpkRvRfsA6Qi+P6giUvUGiOBLAtg4TSVzQyZX2XizrL9khFs4P51POASyU71jWf0QEOWDPkaGbGhsCTXYgRDiFRHPlX+HqChXvljA+odhVSgpbtFmMBIRWgMCurXAbBSSqzIkSTGvTeZfvZZSIjYwqK8kGJCTrOjmNUduBYhWAKB5+daWv0IjrLao1Bx8rlorMUeQn1cukxNGX1zjGJ8rYLZA3UFdBi5kur9uFIERaCVNk3UkKQsNV+LpfdieqaRwoBROkzZKrgdUAHQ/MPZl7u1BRvTrbGBOPOW1GfWxaiOBPIM0zXv765bTBdQfrKjFiIfwXzIIBH7TX/ntJ1PGz+GwtvJH4JqYaJ5DRFoLeWwnB6Usmz2acDYuZ2zO8PNzu8I8qOh6EqCh9GX6KN7zPQUiWzzDp7553vqVZA1u+kc+o+xt7dIc+l/qNDWYsG+iV8oGaY2fymN3GsuE3N6XZ018VYuju8S7PzWW1g0p1R3TgMXk8jU3eZO4lsirX65Rm2Ji7UVxsc8+DRWjQpdwINcXaHD3vz/eSSaxvc7XaeTQVRr+AIIM1vyvKgiWDoXGP8rPakUqTv2yamdrY1ArLkcU3cKgvwxWXXcGUkGH3ADHHndv+9MsageNqjYCpgOadWqBIQ9czkM+j15bowQMKOvdCdTnlv1A7EiL9lUSzVZB+UCr5pfdQQ2J38/X9OVGS/qyDNWKuvImFCmLAdaCNXmVvZIr1D9jwQuI3N89ZvZVDc0iQR4IiP8HOI1H0HaBzg6frt38II180QV8HRfvMaWzMMhcL0iQ/LLT89KQtg2Vks5bCqiV3cLB55HvOsu4UEKboeD3jrRPLwPQWzoaMdXPpGlMJoITSXh5JEfOVqUfEFMFy1YJ7zRXXAaDxKaKAHIGsnK8uW6pD0hEZF0cfED6vOgbIwuAyo24DlSJPGU462HxhnWzCHIcSjNC19Zss1+/orpW928JqZidVaWI2SRYh6GVR998WDqkRi245ZbjmiJS8QdgAXf/rOVN8GYC9TEdAEIpijR2qjV4ng8zcIW7sakd99PXD0D1MLfjjBNYX4aQB7VqLuPGDwO3s7RdHRmHNS2Im575SASsRPsZ97kOfGy2cQTkOay2K6zZ8OT9gNDBtALzoXTB+d3RxgPRiHXszdSzNzBy3OMt4hl+UCEhZQbt9bR4sfUbbktgwx+gbiXZ99503xX4vWk16Q5iGL+vPUVJnU0r5UohQvFJW+bhw/YQUYZFJYjZzV6+sLItLtEUQDR660HJnSDDO8sPUHTRWHUgCUU4K2Eub5mDqmbZBvbBo1yAI7GDapW+H73QOKsC+WvertdXHo5xIyo/0rfXhWRLeufGWOPHesorNtBWsYz5Wi/L1HbSDL/1p/RvN2EKYx0Ecqi0/QxG5GOQ203mjlXB+fqjK8XKbhlKDu2x5N/G87iXMq2K6dMYuyvYGvTU2zKgcFhIyWerzlMWE2TLfo3jdu++n5W/cujJDlB70A+MYJKnooOwj2rELIeyEXWiicXK63Umep7IxzaYcG4wcLuWgVQ+na1kMM1uYzYq0uio/T06Sc1sKw1pisNBDm6fWIfy96DilcLXTUW9840farcaCmh+KpDQxpse9OiXscmMl26P+tNq7+dtc64uFPgxuTzznjbVk/rwL6FuKaWLhYWzik8brxxSFtRuD3YMJ6fLz4owDC8ChBPcBi/4yCMhtBJ6P8sSXR1oU0q7Lhe2DTgJIV4qVAJkbGS3NHKM9xUY6okFxsRXMhvqFsGSHcWeJNX3w0UXmlk/BXddcVFO1VxlN6uXkm0E8aCJvjSuucRhLuQxLHAR4s5/8v/Pxf7GSnmMNcNOrLVx0N/fkRWTS+g1l+cr72rYJAPHn0yu2/JNs6bpuEi1B0qSqbmGFlelxhz0WN6hjm59Xllma1jPC3uLxkEzMqBlIZ/XJCZRn2XhKP7muiYcGMXfG9hnJSiv5LVbo+zcK/VtB0wOaA03uI7osCQz/R3ORUxTQKJg9YAAB7i45d3h0MxonFMkZ6sGsS3/wNGsHEeQiIDIRFZSW1x7pjOD2uWiA5wFTvhhM/f6WhVaU+jHO6mXEa5HSminr+JgaODAs8vCTyjRi6r5AS0TPTKT5GdIhxA+X1FkT2DbB2wb9hW65MtJQG/dGWq+30WoF5PBQS+w3NUzhOzdCmo0Of3tC686MtXnRnns1M70YADbejzTh4y+TlSXJFaHVblFZ4Nl2LnmukWC254UXFK/yzxBv/IIujCRDWXr2acgITc+ZtlgBRoMDTQWGcDxsf/48XL6So/RdjFSt45qLqqlEfsnv0s8IHKnHYa26Uitog2Bx3ZMhXyvWxCxGHz7d5SobBxO+fRwcrDQI+CYsh7vMDmd1fg5VQ1aFU0Pu6OLuE/0N1gbIzEQzdqorAZpEtFdPyGWEXHUJyJVVJ+Jd6HbjY7IaTOlYNySDSd2NX5CuB5AAsUlbgDFiFlBt07z/Yw3dQ4wNesJIouj4LJtMBczfiZGvoLy5PSB8++9iwsYO5z66viX8IrSq1nBUkHqf1A4a+Cfuy+l+VZk2H4K6gX20xqe8whRqvKpzb2TYASkWnC23wsNT2Qvk0McJNqCUnEoEj1nBFRvvaop6EyTsMikx7g+EcIuEx81u7OtEFBeGzlJXiUJjFJpmCBOcWUZy1A+0FT8IZFGG++npcRinP4iBsfeST0Gik0wwoaj5Sz6djC/RrlrSsW3ico4kvd779XM2UazrbZiOJJbCw6Hedu7W5dVGR4bDxlY+/yZSC4K3egtUP9K1KtKVKW0JjYGgTmh4lkIh4ZA7r9DRpOCdZt+FY9QJBmnE3cVzW2ZlM4nv1+oH2rMVPyNKnNnJBDSv/y6I8AnSfhnrT+eplofR47MTGwqsp6ML3r96XTeLqWGrGiO2HGcjRiSSW3AxuksYXxPPM+FWbRYH9kT4LxYzo8CfUwXW/icvyDArmuG2dh40ggvcphTfra14BSu2ppBIS2tIvSeK20Ij7VseGB5dgPKIN6gONr0qvfABbJg/q9JyRAlQpU/iTvdJfRP6qx0OwLcgyKJ/mND7ZvIoCJsmwVg1lJXujkMs6WBUM1B2bhZkiDKsBnRCzmuMCHKJBfaJoY4pfqh8ipKMBEhkYrdleRsXLOb2VwybjSUpU1D9TGNx2lF14eJXMjz7vV5KmCDSPP1JF+da8OYfCZiicpsaq3YG3VVv1YZYIX2AxptyTTuzjSWIBT3nUBddwyHWoxpQzaiRapzvUKP0IeS0/GhTR9V5zk+3XGH2nI25SLa+1yumiGBLZwVIQOLDyWYxNyusb4KBqHD6bdgY6D+VwcJRj+/DLfX8l/UHcDc/r367ogEohYxW5f38s8GtJq4J4hJYRTNzqx14f6EEjLN5QcfYFwV8DHESqOhDfGO0Pl3ZpeVLSNikRF494F72Nvh2qUL0XqAq7zkau1GZhXw0B6GtbG1CfkY/qdzlRIVsrHU42cDIg6pOJIIsk2/GQQmjXhYnFHWumjAiQrpIetsb1Q6G+DRpMoCjf6hDwuQVozIfNk97/vFve4IAbULOqoH3v3yGuPfWCaDiCVDgapezV1AbMfuBiDB+DKuJcK+PiU3aw/IhbVFtHFMfHuEaTlmBVBF3CuR0+binO53Q3r4wSn38Bm+3rVV69aa9GHoft4frt0c/okl5nCoK7XWp/8MAnND5IfC3c6JHKw6Qsj1jB25MsLP135+76EpA3JhbP/QpPy3ZDkIT0Ex+iNnsaOzq0YV92+UWn7/RdUru7SqXGW/AfcRen3GK7Qy5B7x0mtjnVAsIsIGkrPaw==" />
+        </div>
+
+        <div>
+
+            <input type="hidden" name="__VIEWSTATEGENERATOR" id="__VIEWSTATEGENERATOR" value="6A876C66" />
+        </div>
+        <!--=========================== Header ===========================-->
+        <div class="aboutus_header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 text-center logoaboutus">
+                        <img src="../../resources/images/desktop/logoligamansion.png"
+                            alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="aboutus_infocenter">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 text-center">
+                        Pusat Info
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <!--=========================== Bodywork ===========================-->
+
+        <div class="aboutus_mainbody">
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="aboutus_mainbody2">
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <div class="aboutus_left">
+                                        <ul>
+                                            <li class="active"><a href="about-us">Tentang LIGAMANSION</a></li>
+                                            <li class=""><a href="responsible-gaming">Responsible Gambling</a></li>
+                                            <li class=""><a href="faq">Pusat Bantuan</a></li>
+                                            <li class=""><a href="terms-of-use">Syarat dan Ketentuan</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col-sm-9">
+                                    <div class="aboutus_right">
+                                        <div class="aboutus_right_title">Tentang LIGAMANSION</div>
+                                        <div class="aboutus_right_content">
+                                            <h4>TENTANG KAMI</h4>
+                                            <div>
+                                                <p><strong>LIGAMANSION</strong></p>
+                                                <p>Selamat datang di <strong>LIGAMANSION</strong>, situs <a
+                                                        href="https://ligamansion.epizy.com">judi online</a> yang lebih di khususkan seperti permainan Sportsbook, LiveCasino, Slot, Poker, Togel dan Sabung Ayam.
+                                                </p>
+                                                <p><strong>LIGAMANSION</strong> adalah situs permainan taruhan olahraga
+                                                    hingga kasino terkemuka dan terpercaya di Asia,
+                                                    <strong>LIGAMANSION</strong> LIGAMANSION menawarkan pengalaman judi online terbaik dengan berbagai variasi permainan LiveCasino & Sportsbook yang dapat dipilih dengan odds paling kompetitif dalam dunia judi sepak bola. Kami menawarkan Banyak Provider permainan Olahraga yang berbeda setiap bulan dan berbagai kompetisi di seluruh dunia dalam
+                                                    <strong>LIGAMANSION</strong> Sportsbook, Lalu Kami juga menawarkan banyak Provider Permainan LiveCasino dari variasi Baccarat, SicBo, Roulette, BlackJack dan permainan Live Casino lainnya yang dapat dimainkan di
+                                                    <strong>LIGAMANSION</strong>&nbsp;Casino.</p>
+                                                <p>&nbsp;</p>
+                                                <p><strong>KEAMANAN</strong></p>
+                                                <p>Domain aman dan privat serta integritas produk kami adalah poros
+                                                    fundamental dari pengalaman taruhan online di
+                                                    <strong>LIGAMANSION</strong>. Kami selalu mengutamakan keamanan tercanggih dan memperbaharui semua permainan serta proses-proses kami secara berkala, demi memastikan pengalaman permainan online Anda 100% aman dan adil. Kami selalu mengutamakan menjaga kerahasiaan informasi Anda, dan kami tidak akan pernah membagikannya ataupun menjualnya ke pihak ketiga. Karena Keamanan Dan Kenyamanan member adalah tanggung jawab kami.
+</p>
+                                                <p>&nbsp;</p>
+                                                <p><strong>PELAYANAN PELANGGAN</strong></p>
+                                                <p>Kami mempunyai dukungan layanan pelanggan 24 jam nonstop, Staf kami yang ramah dan profesional akan memastikan bahwa semua masalah yang Anda hadapi akan ditangani dengan cepat, efisien, dan secara ramah. Kami memberikan prioritas tinggi untuk memastikan sistem pembayaran yang aman dan terpercaya.
+</p>
+                                                <p>Misi utama kami adalah selalu memberikan pengalaman taruhan judi online bagi member kami. Silahkan hubungi kami melalui Livechat dan Whatsapp, Kami siap membantu anda dan menerima saran atau kritik dari anda.
+</p>
+                                                <p>Kami memilki beberapa metode pembayaran yang mudah dan aman, demi kenyamanan Anda. Kami mengikuti kebijakan kenali pelanggan Anda (KYC) dan anti-pencucian uang (AML), dan kami bekerja sama dengan badan keuangan serta badan pengatur demi memastikan ketaatan berstandar tertinggi pada hukum dan peraturan.
+</p>
+                                                <p>&nbsp;</p>
+                                                <p><strong>PROGRAM &amp; HIBURAN</strong></p>
+                                                <p>Di <strong>LIGAMANSION</strong>,kami berusaha untuk memberikan yang terbaik dalam permainan dan layanan online untuk menawarkan pengalaman unik & terbaik bagi pelanggan kami. Kami memiliki beberapa program berhadiah, yang memberikan pelanggan kami berbagai hadiah yang benar-benar layak untuk para pemain. Bermain di 
+                                                    <strong>LIGAMANSION</strong>&nbsp;tidak hanya menyenangkan dan menghibur, tetapi juga sangat menguntungkan!</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+    </form>
+</body>
+
+</html>
