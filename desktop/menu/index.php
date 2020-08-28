4e2f678554a4ae3a17cf2c9ad83679e3b7de7250
Teguh Ziliwu <teguh.ziliwu@celindo-pratama.com>
2020-08-28 08:06:46 +0700

    28-08-2020 Get image slide from internet

--- desktop/menu/index.php
+++ desktop/menu/index.php
@@ -12,35 +12,28 @@
                 <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="https://api2-fz8.tr8games.com/images/id_cbd_3045f459-f899-4ab1-93df-21297284e3b0_1596014218580.jpg?1596014218580" title="Welcome to FORZA 88 5" alt="Welcome to FORZA 88 5" />
+                    <img src="../../resources/images/desktop/slide1.jpg" title="Welcome to Ligamansion" alt="Welcome to Ligamansion" />
                 </div>
                 <div class="carousel-item">
-                    <img src="https://api2-fz8.tr8games.com/images/id_cbd_466d27a6-3d1b-40fd-a3b7-30f687d42932_1595582929580.jpg?1595582929580" title="Welcome to FORZA88 7" alt="Welcome to FORZA88 7" />
+                    <img src="../../resources/images/desktop/slide2.jpg" title="Welcome to Ligamansion" alt="Welcome to Ligamansion" />
                 </div>
                 <div class="carousel-item">
-                    <img src="https://api2-fz8.tr8games.com/images/id_cbd_05447c22-8b64-4419-9770-7ed553b58c54_1595850047480.jpg?1595850047480" title="Welcome to FORZA 88 4" alt="Welcome to FORZA 88 4" />
+                    <img src="../../resources/images/desktop/slide3.jpg" title="Welcome to Ligamansion" alt="Welcome to Ligamansion" />
                 </div>
                 <div class="carousel-item">
-                    <img src="https://api2-fz8.tr8games.com/images/id_cbd_684bc552-ccd7-4b4e-8e69-de444f1c63a6_1595845587380.jpg?1595845587380" title="Welcome to FORZA88 3" alt="WELCOME to forza88 3" />
-                </div>
-
-
-
-                <div class="carousel-item">
-                    <img src="https://api2-fz8.tr8games.com/images/id_cbd_44a5b62d-c533-437c-978b-fc0808d877f6_1595272403963.png?1595272403963" title="Welcome to FORZA 88 2" alt="Welcome to FORZA 88 2" />
+                    <img src="../../resources/images/desktop/slide4.jpg" title="Welcome to Ligamansion" alt="Welcome to Ligamansion" />
                 </div>
                 <div class="carousel-item">
-                    <img src="https://api2-fz8.tr8games.com/images/id_cbd_aae4dc69-c015-4660-8f21-1beb9d0926d8_1595272435870.png?1595272435870" title="Welcome to FORZA88 1" alt="Welcome to FORZA88 1" />
+                    <img src="../../resources/images/desktop/slide5.png" title="Welcome to Ligamansion" alt="Welcome to Ligamansion" />
                 </div>
                 <div class="carousel-item">
-                    <img src="https://api2-fz8.tr8games.com/images/id_cbd_b71c415f-24d7-4f0a-b206-70d1fea8ba1a_1596698289447.jpeg?1596698289447" title="Welcome to FORZA88 8" alt="Welcome to FORZA88 8" />
+                    <img src="../../resources/images/desktop/slide6.png" title="Welcome to Ligamansion" alt="Welcome to Ligamansion" />
                 </div>
                 <div class="carousel-item">
-                    <img src="https://api2-fz8.tr8games.com/images/id_cbd_aa6670fa-05c8-46a8-85c2-3f427f20e4bc_1595845022593.jpg?1595845022593" title="Welcome to FORZA88 6" alt="Welcome to FORZA88 6" />
+                    <img src="../../resources/images/desktop/slide7.jpg" title="Welcome to Ligamansion" alt="Welcome to Ligamansion" />
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
